613ebbad9d084cb0b37eb2b13ebd92c8803905fb
vipul1243 <148183133+vipul1243@users.noreply.github.com>
2023-10-24 20:45:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,13 +22,13 @@ const PORT = process.env.PORT || 8000;
 connectionDB();
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 // Routes
 app.use("/api/student", userRoute);
